42ef83589b08663d5bf12c475222640c951ea40c
Stuart Kushner <stuartbkushner@gmail.com>
2020-12-14 10:46:53 -0500

    Created cart context

--- src/shared/CartWidget/CartWidget.tsx
+++ src/shared/CartWidget/CartWidget.tsx
@@ -1,3 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import { useCart } from "../../CartContext"
+
+export const CartWidget = () => {
+  const { products } = useCart()
+
+  return (
+    <Link to="/cart">
+      <span className="is-error">{products?.length || 0}</span>
+    </Link>
+  )
+}
